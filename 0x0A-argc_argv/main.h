3a73f3831bd7517f1dcc89133ecf7f8ce9f40ab3
Promisechinaka <promisechinaka47@gmail.com>
2023-01-07 02:12:37 -0800

    main.h

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
+int main(int argc, char *argv[])
+bool is_num(char *argvv)
+int main(int argc, char *argv[])
+
+#endif
